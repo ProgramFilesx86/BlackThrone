2937448d4993689433ad9900e13ea4901efb89e9
NoBody <35371691+CXVVMVII@users.noreply.github.com>
2019-12-05 20:31:16 +0100

    Update mydb.php

--- mydb.php
+++ mydb.php
@@ -1,12 +1,6 @@
 
 <?php
-/*
-*   If you\'re reading this if this helped you don\'t ignore that message
-* Help me with some hacking Tips/Tricks or resources to gain my knowledge
-*         My facebook : https://www.facebook.com/000xPlan
-*              My Github : https://github.com/CXVVMVII
-*                    Made By @WhizzHandrixx
-*/
+
 
 
 
@@ -30,12 +24,3 @@ if($db_con) {
 
 
 ?>
-
-/*
-*   If you\'re reading this if this helped you don\'t ignore that message
-* Help me with some hacking Tips/Tricks or resources to gain my knowledge
-*         My facebook : https://www.facebook.com/000xPlan
-*              My Github : https://github.com/CXVVMVII
-*            My twitter : https://twitter.com/CXXMIXCSII
-*                    Made By @WhizzHandrixx
-*/
